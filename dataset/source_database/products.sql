3ed91d3265b2081004dc03bc19724f33d62c9361
MB <mariusz.brzytwa@gmail.com>
2025-03-09 23:14:58 +0100

    mb_added database sqls

---
+++ dataset/source_database/products.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS [products];
+GO
+CREATE TABLE [products]
+(
+    [product_id] INT IDENTITY(1,1) PRIMARY KEY,   
+    [product_code] VARCHAR(255) NULL,
+    [product_name] VARCHAR(255) NULL,
+    [product_category] VARCHAR(MAX) NULL,
+);
+GO
+
+INSERT INTO [products] ([product_code], [product_name], [product_category])  
+VALUES  
+    ('110', 'Inner tube', 'bike'),  
+    ('120', 'Saddle', 'bike'),  
+    ('130', 'Vacuum cleaner', 'agd'),  
+    ('140', 'Microwave', 'agd'),  
+    ('150', 'Blender', 'agd');  
